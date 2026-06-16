9e3a949bb22f77252d59273dc529c6943892a182
Yeon-Chang <ters9292@gmail.com>
2026-06-17 02:28:01 +0900

    운영 상황 수정 반영

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  webpack(config) {
+    config.resolve.modules = [
+      path.resolve(__dirname, "node_modules"),
+      "node_modules",
+    ];
+    return config;
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "ssl.pstatic.net" },
+      { protocol: "https", hostname: "phinf.pstatic.net" },
     ],
   },
   async headers() {
